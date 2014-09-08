f3973a849316b0ad8003cc30e96aad2ec41bdc26
crismaikon <crismaikonlins@gmail.com>
2014-09-08 14:22:52 -0300

    Tarefa T.01.01 Finalizada

---
+++ src/main/java/br/ufcg/ppgcc/compor/ir/impl/ImpostoDeRenda.java
@@ -0,0 +1,20 @@
+package br.ufcg.ppgcc.compor.ir.impl;
+import java.util.*;
+
+import br.ufcg.ppgcc.compor.ir.*;
+
+public class ImpostoDeRenda implements FachadaExperimento {
+
+	private List<Titular> titulares = new ArrayList<Titular>();
+	
+	public void criarNovoTitular(Titular titular) {
+		
+		titulares.add(titular);
+	}
+
+	public List<Titular> listarTitulares() {
+		
+		return titulares;
+	}
+
+}
