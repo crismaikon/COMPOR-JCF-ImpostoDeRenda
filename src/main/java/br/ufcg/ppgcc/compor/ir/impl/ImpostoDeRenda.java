6df3dcbd11e8ed762db2516e8d70ea24ce446d75
crismaikon <crismaikonlins@gmail.com>
2014-09-08 16:23:31 -0300

    Tarefa T.02.01 finalizada

--- src/main/java/br/ufcg/ppgcc/compor/ir/impl/ImpostoDeRenda.java
+++ src/main/java/br/ufcg/ppgcc/compor/ir/impl/ImpostoDeRenda.java
@@ -1,4 +1,5 @@
 package br.ufcg.ppgcc.compor.ir.impl;
+
 import java.util.*;
 
 import br.ufcg.ppgcc.compor.ir.*;
@@ -6,8 +7,9 @@ import br.ufcg.ppgcc.compor.ir.*;
 public class ImpostoDeRenda implements FachadaExperimento {
 
 	private List<Titular> titulares = new ArrayList<Titular>();
-	
-	
+	private List<FontePagadora> fontePagadores = new ArrayList<FontePagadora>();
+	private Map<Titular, FontePagadora> fontePagadoras = new HashMap<Titular, FontePagadora>();
+
 	public void criarNovoTitular(Titular titular) {
 		if (titular.getNome() == null) {
 			try {
@@ -16,37 +18,46 @@ public class ImpostoDeRenda implements FachadaExperimento {
 				throw new ExcecaoImpostoDeRenda("O campo nome é obrigatório");
 			}
 		}
-		if(titular.getCpf() == null){
+		if (titular.getCpf() == null) {
 			try {
 				throw new ExcecaoImpostoDeRenda("O campo CPF é obrigatório");
 			} catch (Exception e) {
 				throw new ExcecaoImpostoDeRenda("O campo CPF é obrigatório");
 			}
 		}
-		if(titular.getCpf() != "000.000.000-00"){
+		if (titular.getCpf() != "000.000.000-00") {
 			try {
 				throw new ExcecaoImpostoDeRenda("O campo CPF está inválido");
 			} catch (Exception e) {
 				throw new ExcecaoImpostoDeRenda("O campo CPF está inválido");
 			}
 		}
-		
-		
+
 		titulares.add(titular);
 	}
 
 	public List<Titular> listarTitulares() {
-		
+
 		return titulares;
 	}
-	
-	public boolean verificaCpfInvalido(Titular titular){
-		for(Titular a : titulares){
-			if(a.getCpf() == titular.getCpf()){
+
+	public boolean verificaCpfInvalido(Titular titular) {
+		for (Titular a : titulares) {
+			if (a.getCpf() == titular.getCpf()) {
 				return true;
 			}
 		}
 		return false;
+	}
+
+	public void criarFontePagadora(Titular titular, FontePagadora fonte) {
+		fontePagadores.add(fonte);
+
+	}
+
+	public List<FontePagadora> listarFontes(Titular titular) {
+
+		return fontePagadores;
 	}
 
 }
