5709fe72a42f66c53d58965ef7118cd22a81fa22
crismaikon <crismaikonlins@gmail.com>
2014-09-09 11:13:20 -0300

    Arrumando as TAG

--- src/main/java/br/ufcg/ppgcc/compor/ir/impl/ImpostoDeRenda.java
+++ src/main/java/br/ufcg/ppgcc/compor/ir/impl/ImpostoDeRenda.java
@@ -7,9 +7,8 @@ import br.ufcg.ppgcc.compor.ir.*;
 public class ImpostoDeRenda implements FachadaExperimento {
 
 	private List<Titular> titulares = new ArrayList<Titular>();
-	private List<FontePagadora> fontePagadores = new ArrayList<FontePagadora>();
-	
-	private Map<Titular, FontePagadora> fontePagadoras = new HashMap<Titular, FontePagadora>();
+	private List<FontePagadora> fontes = new ArrayList<FontePagadora>();
+	private Map<Titular,List<FontePagadora>> fontePagadoras = new HashMap<Titular,List<FontePagadora>>();
 
 	public void criarNovoTitular(Titular titular) {
 		if (titular.getNome() == null) {
@@ -31,24 +30,21 @@ public class ImpostoDeRenda implements FachadaExperimento {
 		return titulares;
 	}
 
-	public boolean verificaCpfInvalido(Titular titular) {
-		for (Titular a : titulares) {
-			if (a.getCpf() == titular.getCpf()) {
-				return true;
-			}
-		}
-		return false;
-	}
-
 	public void criarFontePagadora(Titular titular, FontePagadora fonte) {
-		fontePagadoras.put(titular, fonte);
-		fontePagadores.add(fonte);
+		
+		if (fontePagadoras.containsKey(titular)){
+			List<FontePagadora> font = fontePagadoras.get(titular);
+			font.add(fonte);
+			
+		}
+		fontes.add(fonte);
+		fontePagadoras.put(titular, fontes);
 
 	}
 
 	public List<FontePagadora> listarFontes(Titular titular) {
 
-		return fontePagadores;
+		return fontes;
 	}
 
 }
